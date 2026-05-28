ee1822097a0c4ac7cb899fbbe4a36b9cde251a94
brose <kharvarashok786@gmail.com>
2026-05-28 19:45:38 +0530

    fixed render voice engine

--- frontend/voice_engine.py
+++ frontend/voice_engine.py
@@ -1,4 +1,4 @@
-import pyttsx3
+from gtts import gTTS
 import uuid
 import os
 
@@ -6,38 +6,30 @@ import os
 # VOICE ENGINE
 # =========================================
 
-engine = pyttsx3.init()
-
-engine.setProperty(
-    "rate",
-    170
-)
-
-engine.setProperty(
-    "volume",
-    1.0
-)
-
-# =========================================
-# SPEAK FUNCTION
-# =========================================
-
 def speak(text):
 
-    os.makedirs(
-        "voices",
-        exist_ok=True
-    )
+    try:
 
-    filename = (
-        f"voices/{uuid.uuid4()}.mp3"
-    )
+        os.makedirs(
+            "voices",
+            exist_ok=True
+        )
 
-    engine.save_to_file(
-        text,
-        filename
-    )
+        filename = (
+            f"voices/{uuid.uuid4()}.mp3"
+        )
 
-    engine.runAndWait()
+        tts = gTTS(
+            text=text,
+            lang="en"
+        )
 
-    return filename
+        tts.save(filename)
+
+        return filename
+
+    except Exception as e:
+
+        print(f"Voice Error: {e}")
+
+        return None
